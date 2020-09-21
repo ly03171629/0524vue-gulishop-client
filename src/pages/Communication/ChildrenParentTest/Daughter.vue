92a5d9bba269aa6f5539f086180e4ce9972c2040
liuyuan <1316425655@qq.com>
2020-09-21 16:49:57 +0800

    slotscope

--- src/pages/Communication/ChildrenParentTest/Daughter.vue
+++ src/pages/Communication/ChildrenParentTest/Daughter.vue
@@ -1,12 +1,14 @@
 <template>
   <div style="background: #ccc; height: 50px;">
     <h3>女儿小红: 有存款: {{money}}</h3>
-    <button>给BABA钱: 100</button>
+    <button @click="giveMoney(100)">给BABA钱: 100</button>
   </div>
 </template>
 
 <script>
+import myMixin from '@/pages/Communication/ChildrenParentTest/myMixin'
 export default {
+  mixins:[myMixin],
   name: 'Daughter',
   data () {
     return {
@@ -14,8 +16,11 @@ export default {
     }
   },
 
-  methods: {
-    
-  }
+  // methods: {
+  //   giveMoney(money){
+  //     this.money -= money
+  //     this.$parent.money += money  //不要滥用  如果一个儿子多个爹，无法使用
+  //   }
+  // }
 }
 </script>
